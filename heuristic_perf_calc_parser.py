164af9d9b57de9bf8c48f1e8a304248e3a2ef876
Hyun Seok Yoon <hyunseoky9@gmail.com>
2026-03-16 18:55:35 -0600

    upload

---
+++ heuristic_perf_calc_parser.py
@@ -0,0 +1,45 @@
+# parse all the heuristic play result files for the cluster.
+import os
+import numpy as np
+
+# what to parse
+envsetting = 20
+idrange = np.arange(0,20)
+# how many sets to make 
+numsets = 10
+
+# example filename: heuristic_play_metapop1_20_RLparam32_RLseed398845_20260316_175238_id2.pkl
+
+# 1. get all heuristic play result files for the given envsetting and date range.
+base_dir = './heuristics_play_results'
+pickle_filenames = []
+for filename in os.listdir(base_dir):
+    if f'_{envsetting}_' in filename:
+        id = int(filename.split('_id')[-1].split('.pkl')[0])
+        if id in idrange:
+            pickle_filenames.append(os.path.join(base_dir, filename))
+            
+
+# 2. divide the filenames into numset lists.
+numfiles_perset = len(pickle_filenames) // numsets
+file_set_list = []
+for i in range(numsets):
+    start_idx = i * numfiles_perset
+    end_idx = (i + 1) * numfiles_perset if i < numsets - 1 else len(pickle_filenames)
+    file_set_list.append(pickle_filenames[start_idx:end_idx])
+
+# write a text file that goes like...
+# set1
+# filename1
+# filename2
+# set2
+# filename3
+# filename4
+# ...
+
+wd = './heuristics_play_results'
+with open(f'{wd}/parseinfo.txt', 'w') as f:
+    for i, file_set in enumerate(file_set_list):
+        f.write(f'set{i+1}\n')
+        for filename in file_set:
+            f.write(f'{os.path.basename(filename)}\n')
--- performance_interpretability_util.py
+++ performance_interpretability_util.py
@@ -1,7 +1,6 @@
 import os 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-
-
+import sys
 import time
 import torch
 import numpy as np
@@ -12,12 +11,11 @@ import os
 import pickle
 import time
 
-def load_episodes(envsetting):
+def load_episodes_human(envsetting):
     '''
     load files with the given environment setting and get list of all filenames and episode dicts.
     '''
     base_dir = './human_play_results/'
-    7
     pickle_filenames = []
     episodes = []
     for filename in os.listdir(base_dir):
@@ -27,14 +25,41 @@ def load_episodes(envsetting):
             with open(os.path.join(base_dir, filename), "rb") as f:
                 ep = pickle.load(f)
                 episodes.append(ep)
-
     return pickle_filenames, episodes
     
+def load_episodes_heuristic(envsetting, heuristic_setid):
+    '''
+    load files with the given environment setting and heuristic set ID and get list of all filenames and episode dicts.
+    '''
+    base_dir = './heuristics_play_results/'
+    parseinfo_file = os.path.join(base_dir, 'parseinfo.txt')
+    with open(parseinfo_file, 'r') as f:
+        lines = f.readlines()
 
-def calc_performance_gap(envsetting):
+    # collect filenames belonging to the requested set
+    target_set = f'set{heuristic_setid}\n'
+    set_filenames = []
+    in_set = False
+    for line in lines:
+        if line == target_set:
+            in_set = True
+        elif line.startswith('set'):
+            in_set = False
+        elif in_set and line.strip():
+            set_filenames.append(line.strip())
+    pickle_filenames = []
+    episodes = []
+    for filename in set_filenames:
+        filepath = os.path.join(base_dir, filename)
+        pickle_filenames.append(filepath)
+        with open(filepath, 'rb') as f:
+            ep = pickle.load(f)
+            episodes.append(ep)
+    return pickle_filenames, episodes
+
+def calc_performance_gap(envsetting, actiontype = 'human', iterations = 1000, info = None):
+
     # setting parameters
-    ## performance gap calculation parameters
-    iterations = 1000
     ## RL policy parameters
     if envsetting == 18:
         config = {'seed': 578396, 'paramset': 30}
@@ -46,7 +71,10 @@ def calc_performance_gap(envsetting):
         raise ValueError(f"Invalid envsetting: {envsetting}.")
 
     # get list of episodes to work on
-    pickle_filenames, episodes = load_episodes(envsetting)
+    if actiontype == 'human':
+        pickle_filenames, episodes = load_episodes_human(envsetting)
+    elif actiontype == 'heuristic':
+        pickle_filenames, episodes = load_episodes_heuristic(envsetting, info['heuristic_setid'])
     
     # load the env and policy
     env = metapop1({'settingID': envsetting})
@@ -95,8 +123,10 @@ def calc_performance_gap(envsetting):
                 stop_accounted_aS = ep['actions'][simstep][env.patchnum:]
             stop_accounted_action = np.concatenate((stop_accounted_aR,stop_accounted_aS))
             ep['L1_a_dist_RLsampled_a_prob'].append(np.abs(stop_accounted_action - ep['RLactions_prob'][simstep]).sum())
-            ep['L1_a_dist_RLsampled_a_prob_R'].append(np.abs(stop_accounted_aR - ep['RLactions_prob'][simstep][0:env.patchnum]).sum())
-            ep['L1_a_dist_RLsampled_a_prob_S'].append(np.abs(stop_accounted_aS - ep['RLactions_prob'][simstep][env.patchnum:]).sum())
+            rlen = env.patchnum if env.kR >= env.patchnum else env.patchnum + 1
+            slen = env.patchnum if env.kS >= env.patchnum else env.patchnum + 1
+            ep['L1_a_dist_RLsampled_a_prob_R'].append(np.abs(stop_accounted_aR - ep['RLactions_prob'][simstep][0:rlen]).sum())
+            ep['L1_a_dist_RLsampled_a_prob_S'].append(np.abs(stop_accounted_aS - ep['RLactions_prob'][simstep][rlen:rlen+slen]).sum())
             # calculate performance gap
             env_og = ep['envcheckpoints'][simstep]
             V_human = 0
@@ -136,16 +166,23 @@ def calc_performance_gap(envsetting):
         ep.pop('envcheckpoints', None)
         ofilename = pickle_filenames[epi].replace(".pkl", "_perfgap_updated.pkl") # add perfgap_updated to filename
         # change directory from ./human_play_results/ to ./human_play_results/performance_gap_calculated/
-        ofilename = ofilename.replace("./human_play_results/", "./human_play_results/performance_gap_calculated/")
+        if actiontype == 'human':
+            ofilename = ofilename.replace("./human_play_results/", "./human_play_results/performance_gap_calculated/")
+        elif actiontype == 'heuristic':
+            ofilename = ofilename.replace("./heuristics_play_results/", "./heuristics_play_results/performance_gap_calculated/")
         with open(ofilename, "wb") as f:
             pickle.dump(ep, f)
         print(f"Saved updated episode with performance gap to {ofilename}")            
 
 if __name__ == "__main__":
-    envsetting = 21 # 18(n5), 20(n10), 21(n20) 
-        
     t0 = time.time()
-    calc_performance_gap(envsetting)
+    envsetting = 20 # 18(n5), 20(n10 median centrality), 21(n20)
+    actiontype =  'heuristic'
+    if actiontype == 'heuristic':
+        info = {'heuristic_setid' : int(sys.argv[1])}
+    else:
+        info = None
+    calc_performance_gap(envsetting, actiontype = actiontype, iterations = 10, info = info)
     #calc_performance_gap(2) # for testing, use runid=1
     elapsed = time.time() - t0
     print(f"Done. Elapsed time: {elapsed:.2f} seconds ({elapsed/60:.2f} minutes)")
